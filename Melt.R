7de45807677d08d29ec36e6888d13882cf26eaf2
Abid Shaikh <abidalishaikh@hotmail.com>
2022-02-05 14:23:05 +0500

    Melt.R

--- Melt.R
+++ Melt.R
@@ -1,15 +1,11 @@
-#following function returns rowMeans of selected columns with 5 likert levels returning only selected columns (converting to num and rounding 2 digits)
+#following function returns molten value col with 5 likert levels 
 Melt <- function(s){
-lvls <- c("Strongly Disagree","Disagree","uncertain","Agree","Strongly Agree")
-q <- read.csv("surveys/SCEQ/Questionnaires.csv",stringsAsFactors = F)
+  lvls <- c("Strongly Disagree"=1,"Disagree"=2,"uncertain"=3,"Agree"=4,"Strongly Agree"=5)
+  q <- read.csv("surveys/SCEQ/Questionnaires.csv",stringsAsFactors = F)
 
 s2=sapply(s[,names(s)%in%q$Qshort],as.character)
-
-l=1
-for (i in lvls ){
-  s2[s2==i] <- l
-  l=l+1
-}
+for (i in lvls )
+  s2[s2==names(lvls[i])] <- (lvls[i])
 
 s2=apply(s2,2,as.integer)
 s1 <- s[,!(names(s)%in%q$Qshort)]
@@ -21,7 +17,4 @@ molten$Indicator <- gsub("[0-9]","",molten$variable)
 
 return(molten)
 }
-
-
-
 
